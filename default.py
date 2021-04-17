1164e77b5ddf525e866ce2a8f76ebca9d3fe071d
Dipen Shah <dipenmshah8@gmail.com>
2021-04-17 14:43:44 -0400

    add proper business logic

--- default.py
+++
@@ -1,7 +0,0 @@
-import sys
-from random_person_api import random_person_api
-
-print(str(sys.argv))
-
-#random_person_api('create')
-#random_person_api('json', 'file')
---
+++ random_person_api/random_person_api.py
@@ -0,0 +1,16 @@
+import make_request_call
+import constant
+import json
+import sys
+
+def random_person_api():
+    data = make_request_call.make_request_call(constant.URL)
+    if (len(sys.argv) > 2) and sys.argv[1] == 'json':
+        with open(sys.argv[2]+'.json', 'w') as outfile:
+            json.dump(data, outfile)
+    elif sys.argv[1] == 'create':
+        print(data)
+    else:
+        print("ERR: Please specify the correct arguments")
+
+random_person_api()
