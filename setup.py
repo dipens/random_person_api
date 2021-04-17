615d45256935e2442b52a896718ec3867ac6c207
Dipen Shah <dipenmshah8@gmail.com>
2021-04-17 14:42:39 -0400

    added proper business logic

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(name='random_person_api',
 version='0.1',
 description='Random Person Generator API',
-url='#',
+url='https://github.com/dipens/random_person_api',
 author='Dipen Shah',
 author_email='dipen.shah@alayacare.com',
 license='MIT',
