e6217c11fe93f5a1907c64704d2ef3ec2a3b93db
Dipen Shah <dipenmshah8@gmail.com>
2021-06-28 11:09:12 -0400

    black magic python

--- random_person_api/SessionManager.py
+++ random_person_api/SessionManager.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
+
 class SessionManager:
     def __init__(self):
         pass
+
     def get_session(self):
         self.engine = create_engine("sqlite:///example.db")
         self.session = Session(self.engine, future=True)
